d7e7af00e8400c5ad37f56cd1e12e676345f9a9b
Konrad Hinsen <hinsen@cnrs-orleans.fr>
2011-04-15 18:48:22 +0200

    AOT compilation

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clj-hdf5 "0.1"
   :description "HDF5 interface for Clojure based on JHDF5"
   :url "http://code.google.com/p/clj-hdf5/"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :aot [clj-hdf5.core]
+  :dependencies [[org.clojure/clojure "1.2.1"]
 		 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
